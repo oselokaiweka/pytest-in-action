0be145dd8378da2aa9dab01dc67fbe89804c7259
Oseloka I <115153490+oselokaiweka@users.noreply.github.com>
2024-06-21 11:38:03 +0000

    Adding test for cli.py and mkchange.py using test_cli.py.

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from .web import app
+from .cli import make_change
 from .greeting import my_name
 from .smath import add, subtract
 from .hello import more_hello, more_bye
---
+++ tests/test_cli.py
@@ -0,0 +1,28 @@
+import pytest
+from click.testing import CliRunner
+
+from mylib.mkchange import change
+from src.cli import make_change
+
+
+@pytest.fixture
+def runner():
+    yield CliRunner()
+
+@pytest.fixture
+def amount():
+    yield [{5: "quarters"}, {1: "nickels"}, {4: "pennies"}]
+
+
+def test_change(amount):
+    assert amount == change(1.34)
+
+def test_cli_help(runner):
+    result = runner.invoke(make_change, ["--help"])
+    assert result.exit_code == 0
+    assert "Usage" in result.output
+
+def test_cli_change(runner):
+    result = runner.invoke(make_change, ["--amount", "1.34"])
+    assert result.exit_code == 0
+    assert "5" in result.output
